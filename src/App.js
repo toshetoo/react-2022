dab434147327bd4bae0ded0445a30b799b03b498
Todor Ivanov <ivanov.todor1@gmail.com>
2022-10-21 12:12:52 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import { Layout } from './components/layout/Layout';
 import { Routes, Route } from 'react-router';
 import { UsersList } from './components/users/users-list/UsersList';
 import { UserForm } from './components/users/user-form/UserForm';
+import { Login } from './components/auth/login/Login';
+import { NonAuthenticatedRoute } from './utils/guards/NonAuthenticatedRoute';
+import { AuthenticatedRoute } from './utils/guards/AuthenticatedRoute';
+import { Register } from './components/auth/register/Register';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Layout />} >
+        <Route path="/login" element={<NonAuthenticatedRoute> <Login /> </NonAuthenticatedRoute>} />
+        <Route path="/register" element={<NonAuthenticatedRoute> <Register /> </NonAuthenticatedRoute>} />
+        <Route path="/" element={<AuthenticatedRoute> <Layout /> </AuthenticatedRoute>} >
           <Route path="users" element={<UsersList />} />
           <Route path="users/create" element={<UserForm />} />
+          <Route path="users/edit/:id" element={<UserForm />} />
         </Route>
       </Routes>
     </div>
